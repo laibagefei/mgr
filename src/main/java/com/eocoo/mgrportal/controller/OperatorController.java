90adc4e28ec30190fe2fa27a8612f95bbc155f9a
laibagefei <laibagefei@163.com>
2018-07-31 23:16:49 +0800

    add OperatorController

---
+++ src/main/java/com/eocoo/mgrportal/controller/OperatorController.java
@@ -0,0 +1,15 @@
+package com.eocoo.mgrportal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OperatorController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+
+}
