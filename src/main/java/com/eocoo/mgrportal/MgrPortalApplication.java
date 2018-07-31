6a0248ffea738d1532cbd2c71ce5ff7599716fa4
laibagefei <laibagefei@163.com>
2018-07-31 22:55:48 +0800

    commit src file ---by gefei

---
+++ src/main/java/com/eocoo/mgrportal/MgrPortalApplication.java
@@ -0,0 +1,12 @@
+package com.eocoo.mgrportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgrPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MgrPortalApplication.class, args);
+    }
+}
